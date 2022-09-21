04310a4a28151ea039388d50adf827647eab41e0
vdev3094 <66820680+vdev3094@users.noreply.github.com>
2022-09-21 16:31:05 +0300

    Revert "Revert "prisma models instantiated""
    This reverts commit f253b3a1a8f6d458ca5aac85bfef0dc8ab051266.

---
+++ src/app.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { OrderModule } from './order/order.module';
+
+@Module({
+  imports: [AuthModule, UserModule, OrderModule],
+})
+export class AppModule {}
---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('signup')
+  public signup() {
+    this.authService.signup();
+  }
+
+  @Post('signin')
+  public signin() {
+    this.authService.signin();
+  }
+}
